6881362172436af9b6167c8cdf0df3ecde0830e8
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-04-15 18:17:59 +1000

    recompile

---
+++ index.d.ts
@@ -0,0 +1,2 @@
+export { PeekModuleFactory } from "./peek-util/peek-module-web.factory";
+export { Platform } from "./peek-util/platform-web";
---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var peek_module_web_factory_1 = require("./peek-util/peek-module-web.factory");
+exports.PeekModuleFactory = peek_module_web_factory_1.PeekModuleFactory;
+var platform_web_1 = require("./peek-util/platform-web");
+exports.Platform = platform_web_1.Platform;
+//# sourceMappingURL=/Users/jchesney/project/peek-util-web/index.js.map
